abda9bd0a1b2c0abe02026a78c8314ac779c567a
alejandro-programador2 <programador2@booksandbooksdigital.com.co>
2022-11-10 16:34:14 -0500

    Creados test para el componente Col

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-import { CheckBox, CheckBoxGroup } from 'components/CheckBox'
-
 export const App = () => {
-  const onCheck = (value) => console.log(value)
-
-  return (
-    <CheckBoxGroup legend='Grupo de prueba' onChecked={onCheck}>
-      <CheckBox type='checkbox' label='Opción 1' state='normal' />
-      <CheckBox type='checkbox' label='Opción 2' state='normal' />
-      <CheckBox type='checkbox' label='Opción 3' state='normal' />
-    </CheckBoxGroup>
-  )
+  return <h1>UI-components</h1>
 }
